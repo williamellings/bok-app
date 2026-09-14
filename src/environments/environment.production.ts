b832fd48fd1446e86d8cb2726e0efb7d7ba042d9
williamellings <wille.ellings@hotmail.com>
2026-09-14 12:32:34 +0200

    Fix production API URL

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://bokapi-william.azurewebsites.net/api'
+  apiUrl: 'https://bokapi-william-ftgybjcph7hwakhe.swedencentral-01.azurewebsites.net/api'
 };
